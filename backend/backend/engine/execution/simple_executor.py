2f4e5d2d5a5178dabd8a18b9752903abecc10b1f
Leonard Geißler <60317151+GodBambusleitung@users.noreply.github.com>
2025-05-20 17:57:52 +0200

    change transformer type

--- backend/backend/engine/execution/simple_executor.py
+++ backend/backend/engine/execution/simple_executor.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 from collections.abc import Sequence
 
 import numpy as np
-from lark import ParseTree, Token, Transformer
+from lark import ParseTree, Token, Transformer_NonRecursive
 from loguru import logger
 from numpy import uint32
 
@@ -14,7 +14,7 @@ from .common import ColResult, DocResult, TResult, junction, negation
 from .executor import Executor
 
 
-class SimpleExecutor(Transformer[Token, DocResult], Executor):
+class SimpleExecutor(Transformer_NonRecursive[Token, DocResult], Executor):
     """This transformer evaluates a parse tree bottom-up and computes the query result."""
 
     fainder_mode: FainderMode
