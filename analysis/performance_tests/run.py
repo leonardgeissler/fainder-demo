fd088cf64bf49c3342afdd45237ae847e79e234f
Leonard Geißler <60317151+GodBambusleitung@users.noreply.github.com>
2025-06-07 13:51:07 +0200

    fix

--- analysis/performance_tests/run.py
+++ analysis/performance_tests/run.py
@@ -60,7 +60,7 @@ def initialize_engines(config: PerformanceConfig) -> Dict[str, Engine]:
         rebinning_path=settings.rebinning_index_path,
         conversion_path=settings.conversion_index_path,
         histogram_path=settings.histogram_path,
-        num_workers=config.fainder.max_workers,
+        num_workers=config.fainder.max_workers - 1,
         num_chunks=config.fainder.max_workers - 1,
         chunk_layout=config.fainder.chunk_layout,
     )
